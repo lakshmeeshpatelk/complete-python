ad1ad6ded3282e7aec43a3402c120a01bfa1f1b1
rareeaseadmin <rareeaseoficial@gmail.com>
2026-06-21 00:39:32 +0530

    variables , data types and string methods in python

---
+++ complete-python.py
@@ -0,0 +1,147 @@
+
+# This is a comment in python
+print("Hello world!")
+print("I love pizza")
+
+# variables
+
+
+#String variables
+first_name = "Bro"
+print(first_name)
+email = "Bro@gmail.com"
+print(email)
+food= "pizza"
+print(food)
+
+print(type (food))
+print(f"the email is {email}")
+
+
+
+# string concatanation = joining two strings by adding them
+second_name = "code"
+print(second_name)
+
+full_name = first_name + " " + second_name
+print(full_name)
+
+print(type(full_name))
+
+
+#integer
+age = 21
+age += 2
+print(age)
+
+# print("age = "  + age)
+print(f"The age is {age}")
+
+#floating point aks decimal
+
+
+cgpa = 7.5
+print(cgpa)
+print(type(cgpa))
+
+
+print(f"{cgpa + age}")
+
+height = str(6.3)
+
+print("height =" + height)
+
+name = "lakshmeesh"
+
+second_name = "patel"
+
+middle_name = name + " " + second_name
+
+print(middle_name)
+
+
+# Boolean data type
+
+is_student = True
+print(is_student)
+
+is_raining = False
+print(is_raining)
+
+
+#multiple  variables in a single line
+college , section , branch =  "CIT" , "A", "ECE"
+print(college, section , branch)
+
+
+#string methods
+
+friend_name = "sharan navada"
+print(len(friend_name))
+print(friend_name.capitalize())
+print(friend_name.upper())
+print(friend_name.lower())
+print(friend_name.isdigit())
+print(friend_name.isalpha())
+print(friend_name.find("n"))
+print(friend_name.replace("n","m"))
+print(friend_name.startswith("s"))
+print(friend_name.endswith("a"))
+print(friend_name*7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
